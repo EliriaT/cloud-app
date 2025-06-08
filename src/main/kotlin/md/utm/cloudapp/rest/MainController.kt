68457e42ec4ba8fc05d36dffdf2660a9fc8df4e1
IrinaT <irina.tiora@isa.utm.md>
2025-06-09 02:14:04 +0300

    Add some dummy logging about hostname machine on default home endpoint

--- src/main/kotlin/md/utm/cloudapp/rest/MainController.kt
+++ src/main/kotlin/md/utm/cloudapp/rest/MainController.kt
@@ -2,12 +2,19 @@ package md.utm.cloudapp.rest
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.net.InetAddress
+import org.slf4j.LoggerFactory
 
 @RestController
 class MainController {
-
+    companion object {
+        private val logger = LoggerFactory.getLogger(MainController::class.java)
+    }
     @GetMapping("/")
     fun main(): String {
-        return "Hello World! Version 4! Testing rollback~!"
+        val hostname = InetAddress.getLocalHost().hostName
+
+        logger.info("Request received on host: {}", hostname)
+        return "Hello World! Version 4! Host: $hostname"
     }
 }
